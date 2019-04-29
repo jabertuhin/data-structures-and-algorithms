6d0d71934476e054b0a956277624fa30b2d051b7
jabertuhin <jabertuhin@gmail.com>
2019-04-29 16:15:53 +0600

    queue added.

---
+++ data_structures/queue.py
@@ -0,0 +1,44 @@
+"""
+This code was a part of a Udacity course[DS and Algorithms in python] Quiz.
+"""
+
+class Queue:
+    def __init__(self, head=None):
+        self.storage = [head]
+
+    def enqueue(self, new_element):
+        self.storage.append(new_element)
+
+    def peek(self):
+        return self.storage[0]
+
+    def dequeue(self):
+        try:            
+            return self.storage.pop(0)        
+        except:
+            return "Queue is empty."
+    
+# Setup
+q = Queue(1)
+q.enqueue(2)
+q.enqueue(3)
+
+# Test peek
+# Should be 1
+print(q.peek())
+
+# Test dequeue
+# Should be 1
+print(q.dequeue())
+
+# Test enqueue
+q.enqueue(4)
+# Should be 2
+print(q.dequeue())
+# Should be 3
+print(q.dequeue())
+# Should be 4
+print(q.dequeue())
+q.enqueue(5)
+# Should be 5
+print(q.peek())
