05b7cb36fb84a1407d84556dbd5db3dfb049d5cd
Jaber <jabertuhin@gmail.com>
2019-04-30 22:05:21 +0600

    linked_list added with singly_list.

--- data_structures/__init__.py
+++ data_structures/__init__.py
@@ -1,1 +1,2 @@
 from .array import right_shift
+from .linked_list import singly_linked_list
---
+++ data_structures/linked_list/singly_linked_list.py
@@ -0,0 +1,8 @@
+class Node:
+    def __init__(self, val, next):
+        self.val = val
+        self.next = next
+
+
+def max_val():
+    pass
