b8c6eaf8ef2911ae42943496c8edd94d1504daac
Tahmid Choyon <tahmid.choyon@gmail.com>
2019-05-12 19:19:06 +0600

    Reformat code

--- data_structures/queue.py
+++ data_structures/queue.py
@@ -2,43 +2,56 @@
 This code was a part of a Udacity course[DS and Algorithms in python] Quiz.
 """
 
+
 class Queue:
     def __init__(self, head=None):
-        self.storage = [head]
+        self.storage = [head] if head is not None else []  # no need to insert when head is None
 
     def enqueue(self, new_element):
         self.storage.append(new_element)
 
     def peek(self):
-        return self.storage[0]
+        if len(self.storage) > 0:
+            return self.storage[0]
+        raise IndexError("queue is empty")
 
     def dequeue(self):
-        try:            
-            return self.storage.pop(0)        
-        except:
-            return "Queue is empty."
-    
-# Setup
-q = Queue(1)
-q.enqueue(2)
-q.enqueue(3)
+        if len(self.storage) > 0:
+            return self.storage.pop(0)
+        raise IndexError("queue is empty")
 
-# Test peek
-# Should be 1
-print(q.peek())
+    def __len__(self):
+        return len(self.storage)
 
-# Test dequeue
-# Should be 1
-print(q.dequeue())
+    def __str__(self):
+        return str(self.storage)
 
-# Test enqueue
-q.enqueue(4)
-# Should be 2
-print(q.dequeue())
-# Should be 3
-print(q.dequeue())
-# Should be 4
-print(q.dequeue())
-q.enqueue(5)
-# Should be 5
-print(q.peek())
+    def __repr__(self):
+        return self.__str__()
+
+
+if __name__ == '__main__':
+    # Setup
+    q = Queue(1)
+    q.enqueue(2)
+    q.enqueue(3)
+
+    # Test peek
+    # Should be 1
+    print(q.peek())
+
+    # Test dequeue
+    # Should be 1
+    print(q.dequeue())
+
+    # Test enqueue
+    q.enqueue(4)
+    # Should be 2
+    print(q.dequeue())
+    # Should be 3
+    print(q.dequeue())
+    # Should be 4
+    print(q.dequeue())
+    q.enqueue(5)
+    # Should be 5
+    print(q.peek())
